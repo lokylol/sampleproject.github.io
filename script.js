a83eff8a1f8063e8ee7e5e091948bccf6847bb72
lokylol <70518367+lokylol@users.noreply.github.com>
2021-04-20 18:02:55 +0530

    mysampleproject

---
+++ script.js
@@ -0,0 +1,40 @@
+// Get the current year for the copyright
+$("#year").text(new Date().getFullYear());
+
+// Init Scrollspy
+$("body").scrollspy({ target: "#main-nav" });
+
+// Smooth Scrolling
+$("#main-nav a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
